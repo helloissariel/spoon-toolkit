7e88ea45568335fc9cf2e73dc2c39548a19c9619
airplane <weixiaole@nbicc.com>
2025-05-29 19:49:51 +0800

    feat: fluence into __init__.py

--- spoon_toolkits/__init__.py
+++ spoon_toolkits/__init__.py
@@ -1,2 +1,35 @@
 # Spoon Toolkits - Comprehensive blockchain and cryptocurrency tools
-__version__ = "0.1.0" 
+__version__ = "0.1.0"
+
+from .fluence.fluence_tools import (
+    FluenceListSSHKeysTool,
+    FluenceCreateSSHKeyTool,
+    FluenceDeleteSSHKeyTool,
+    FluenceListVMsTool,
+    FluenceCreateVMTool,
+    FluenceDeleteVMTool,
+    FluencePatchVMTool,
+    FluenceListDefaultImagesTool,
+    FluenceEstimateVMTool,
+    FluenceListBasicConfigurationsTool,
+    FluenceListCountriesTool,
+    FluenceListHardwareTool,
+    SearchFluenceMarketplaceOffers
+)
+
+
+__all__ = [
+    "FluenceListSSHKeysTool",
+    "FluenceCreateSSHKeyTool",
+    "FluenceDeleteSSHKeyTool",
+    "FluenceListVMsTool",
+    "FluenceCreateVMTool",
+    "FluenceDeleteVMTool",
+    "FluencePatchVMTool",
+    "FluenceListDefaultImagesTool",
+    "FluenceEstimateVMTool",
+    "FluenceListBasicConfigurationsTool",
+    "FluenceListCountriesTool",
+    "FluenceListHardwareTool",
+    "SearchFluenceMarketplaceOffers",
+]
