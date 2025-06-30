41d149a4811016b8dc46f394a0023bc9487d5c00
Hecate2 <2474101468@qq.com>
2025-06-30 17:31:38 +0800

    extra args in historical balances

--- spoon_toolkits/the_graph_token_api/historical_balances.py
+++ spoon_toolkits/the_graph_token_api/historical_balances.py
@@ -1,3 +1,4 @@
+import datetime
 from fastmcp import FastMCP
 from .http_client import the_graph_token_api_client
 from .utils import normalize_ethereum_contract_address
@@ -5,12 +6,26 @@ from .utils import normalize_ethereum_contract_address
 mcp = FastMCP("TheGraphTokenApiHistoricalBalances")
 
 @mcp.tool()
-async def historical_balances(address: str, network_id: str = "mainnet"):
+async def historical_balances(
+        address: str, network_id: str = "mainnet",
+        interval: str = '1h', contracts: str = '',
+        start_time: int = 0, end_time: int = 0):
     """
     Get the historical ERC-20 and native ether balances of an address.
     network_id: arbitrum-one, avalanche, base, bsc, mainnet, matic, optimism, unichain
+    interval: 1h, 4h, 1d, 1w
+    start_time: UNIX timestamp in seconds
+    end_time: UNIX timestamp in seconds. If the input is 0, will use current time.
     """
     address = normalize_ethereum_contract_address(address)
-    resp = await the_graph_token_api_client.get(f"/historical/balances/evm/{address}?network={network_id}")
+    url = f"/historical/balances/evm/{address}?network={network_id}&interval={interval}"
+    if end_time == 0:
+        end_time = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
+    url += f"&end_time={end_time}"
+    if start_time:
+        url += f"&start_time={start_time}"
+    if contracts:
+        url += f"&contracts={contracts}"
+    resp = await the_graph_token_api_client.get(url)
     resp = resp.json()
     return resp
