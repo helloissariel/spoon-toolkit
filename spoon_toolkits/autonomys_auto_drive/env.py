4a5b9a17b6b0256d7e25760c72383c86e421b0a4
moonpyt <moon1179797619@gmail.com>
2025-07-22 16:46:10 +0800

    refactor directory structure

--- spoon_toolkits/autonomys_auto_drive/env.py
+++
@@ -1,6 +0,0 @@
-import os
-import dotenv
-dotenv.load_dotenv()
-
-AUTONOMYS_AUTO_DRIVE_API_KEY = os.environ.get('AUTONOMYS_AUTO_DRIVE_API_KEY', '')
-AUTONOMYS_AUTO_DRIVE_AUTH_PROVIDER = os.environ.get('AUTONOMYS_AUTO_DRIVE_AUTH_PROVIDER', 'apikey')
